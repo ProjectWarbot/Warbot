3ba21b043b0d0ef6a55224f0dea1f608131c4a2f
TsubameDono <sebastien.beugnon.pro@gmail.com>
2015-06-19 09:33:33 +0200

    - Update Javadoc for edu.warbot.agents.actions.*

--- src/main/java/edu/warbot/agents/actions/AgressiveActionsMethods.java
+++ src/main/java/edu/warbot/agents/actions/AgressiveActionsMethods.java
@@ -2,6 +2,15 @@ package edu.warbot.agents.actions;
 
 import edu.warbot.agents.actions.constants.AgressiveActions;
 
+/**
+ * Définition des actions agressives par méthode
+ * <p/>
+ * Double utilisation :
+ * <ul>
+ * <li> Dans les agents Madkit pour définir le code lors d'un appel d'Activator</li>
+ * <li> Dans les "cerveaux" (brains) pour cacher la constante Action</li>
+ * </ul>
+ */
 public interface AgressiveActionsMethods extends AgressiveActions {
 
     String fire();
--- src/main/java/edu/warbot/agents/actions/IdlerActionsMethods.java
+++ src/main/java/edu/warbot/agents/actions/IdlerActionsMethods.java
@@ -2,6 +2,15 @@ package edu.warbot.agents.actions;
 
 import edu.warbot.agents.actions.constants.IdlerActions;
 
+/**
+ * Définition des actions de base par méthode
+ * <p/>
+ * Double utilisation :
+ * <ul>
+ * <li> Dans les agents Madkit pour définir le code lors d'un appel d'Activator</li>
+ * <li> Dans les "cerveaux" (brains) pour cacher la constante Action</li>
+ * </ul>
+ */
 public interface IdlerActionsMethods extends IdlerActions {
 
     String idle();
